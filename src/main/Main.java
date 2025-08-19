e7cc91d600a9f96d048bb15da951f777cd3a0307
Erik Nascimento <eriknascimento035@gmail.com>
2025-08-19 10:59:05 -0300

    Criação da Exception para quando a lista de espera estiver vazia.

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,5 @@ public class Main {
     public static void main(String[] args) {
         Login login = new Login();
         login.menu();
-
-        System.out.println("Erik Nascimento da Silva");
     }
 }
