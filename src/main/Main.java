0f64b474b4cfbd7d628b840312d877be0a653e7a
Erik Nascimento <eriknascimento035@gmail.com>
2025-08-19 10:47:39 -0300

    Teste Erik Nascimento da Silva

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,5 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Login login = new Login();
         login.menu();
+
+        System.out.println("Erik Nascimento da Silva");
     }
 }
